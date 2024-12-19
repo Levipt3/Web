95b0cadefe78d026868fe0dce46e9b93247ec494
Levipt3 <ll2581837731@outlook.com>
2024-12-19 17:22:46 +0800

    Web v0.2

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -17,6 +17,31 @@ def about():
 @app.route('/music')
 def music():
     return render_template('/music.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('/register.html')
+    else:
+        # 处理注册逻辑
+        username = request.form.get('username')
+        password = request.form.get('password')
+        sex = request.form.get('sex')
+        hobby = request.form.getlist('hobby')
+        city = request.form.get('city')
+        print(username, password, sex, hobby, city)
+        # 处理注册逻辑
+        return '注册成功'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('/login.html')
+    else:
+        print(request.form.get('username'), request.form.get('password'))
+        return '登录成功'
 
 
 if __name__ == '__main__':
